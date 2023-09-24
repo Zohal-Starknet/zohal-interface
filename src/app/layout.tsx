ddf1128fe2b09fb534ed3d5aa857fb7da3ba0fcf
Aria Kassaei <arikassaei@gmail.com>
2023-09-24 09:27:02 +0200

    💄 Adding main layout to the page (#3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,20 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import Providers from "./providers";
+import { Roboto_Mono } from "next/font/google";
+import Header from "./header";
 
 export const metadata: Metadata = {
   title: "Satoru",
   description: "Satoru",
 };
+
+// TODO - Link this to the Tailwind Font
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal", "italic"],
+});
 
 export default function RootLayout({
   children,
@@ -13,9 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={robotoMono.className}>
       <body>
-        <Providers>{children}</Providers>
+        <div className="flex flex-col h-full">
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Panel from "./ui/panel";
+
 export default function Home() {
-  return <div>SATORU</div>;
+  return (
+    <main className="flex flex-auto gap-3 p-3 h-full">
+      <div className="flex flex-auto flex-col gap-3 rounded-md">
+        <Panel className="flex-auto">Chart</Panel>
+        <Panel className="flex-initial basis-[20rem]">Position</Panel>
+      </div>
+      <Panel className="w-80">Trade / Swap</Panel>
+    </main>
+  );
 }
