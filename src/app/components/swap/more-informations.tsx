4eafd18d6fda8b0e2f86391aba81e9356ea1b67c
Aria Kassaei <arikassaei@gmail.com>
2023-10-01 19:56:40 +0200

    💄 Adding More Infos collapsable content (#8)
    * adding more infos collapsable content
    
    * resolving conflicts into lock file
    
    * removed unuseful state in more informations component
    
    ---------
    
    Co-authored-by: Yohan Tancrez <yohantancrez@gmail.com>

---
+++ src/app/components/swap/more-informations.tsx
@@ -0,0 +1,66 @@
+"use client";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { ChevronRight } from "@satoru/app/ui/icons";
+import { useState } from "react";
+
+// TODO - Export Chevron animated in its own component
+export default function SwapMoreInformations() {
+  const [open, setOpen] = useState<boolean | undefined>(undefined);
+
+  return (
+    <Collapsible.Root
+      open={open}
+      onOpenChange={(openValue) => {
+        setOpen(openValue);
+      }}
+      className="mt-5 pl-1"
+    >
+      <Collapsible.CollapsibleTrigger className="w-full">
+        <div className="flex items-center justify-between">
+          <span>More informations</span>
+          <ChevronRight
+            label={open ? "Hide" : "Show"}
+            className={`text-white text-right ${
+              open === undefined
+                ? ""
+                : open
+                ? "animate-[rotate90_200ms_ease_forwards]"
+                : "animate-[rotate0_200ms_ease_forwards]"
+            }`}
+          />
+        </div>
+      </Collapsible.CollapsibleTrigger>
+      <Collapsible.CollapsibleContent className="mt-4 overflow-hidden data-[state=open]:animate-[slideDown_200ms_ease] data-[state=closed]:animate-[slideUp_200ms_ease]">
+        <div className="flex flex-col gap-2 pr-2">
+          {priceInfos.map((info, index) => (
+            <PriceInfo key={index} {...info} />
+          ))}
+        </div>
+      </Collapsible.CollapsibleContent>
+    </Collapsible.Root>
+  );
+}
+
+// TODO - Do real Price informations based on User selection and input
+const priceInfos = [
+  { label: "Token 1 Price", value: "$26,146.00" },
+  { label: "Token 2 Price", value: "$0.818" },
+  { label: "Available Liquidity", value: "$272,569.02" },
+  { label: "Price", value: "31913.0674 ARB / BTC" },
+];
+
+type PriceInfoProps = {
+  label: string;
+  value: string;
+};
+
+function PriceInfo(props: PriceInfoProps) {
+  const { label, value } = props;
+
+  return (
+    <div className="flex">
+      <span className="text-xs text-[#ffffffb3]">{label}</span>
+      <span className="ml-auto text-xs">{value}</span>
+    </div>
+  );
+}
