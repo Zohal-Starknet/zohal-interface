3a5c6bbf4d6a7082bf63a251f451ef9bf0e7415b
YohanTz <yohantancrez@gmail.com>
2023-10-12 17:30:01 +0200

    removed todo comment

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,6 +1,5 @@
 import { Roboto_Mono } from "next/font/google";
 
-// TODO - Link this to the Tailwind Font
 export const robotoMono = Roboto_Mono({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
