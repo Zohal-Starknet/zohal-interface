5ecd47f88002100a40de5ac9fa707d013fcb045c
Aria Kassaei <arikassaei@gmail.com>
2023-10-26 23:25:23 +0200

    extract logic into hook

---
+++ src/app/hooks/token-input.ts
@@ -0,0 +1,59 @@
+import { useCallback, useRef, useState } from "react";
+
+type Props = {
+  /** Ratio applied to the prices when modified */
+  ratio: number;
+};
+
+export function useTokenInputs(props: Props) {
+  const { ratio } = props;
+
+  // TODO: Type this state stronger than string
+  const [payTokenSymbol, setPayTokenSymbol] = useState("ETH");
+  const [payTokenValue, setPayTokenValue] = useState("");
+
+  // TODO: Type this state stronger than string
+  const [receiveTokenSymbol, setReceiveTokenSymbol] = useState("BTC");
+  const [receiveTokenValue, setReceiveTokenValue] = useState("");
+
+  const temporaryPayTokenValueRef = useRef("");
+  const temporaryPayTokenSymbolRef = useRef("");
+
+  const updatePayTokenValue = useCallback(function updatePayTokenValue(
+    tokenValue: string,
+  ) {
+    setPayTokenValue(tokenValue);
+    setReceiveTokenValue(
+      tokenValue !== "" ? (parseFloat(tokenValue) * ratio).toString() : "",
+    );
+  }, []);
+
+  const updateReceiveTokenValue = useCallback(function updateReceiveTokenValue(
+    tokenValue: string,
+  ) {
+    setReceiveTokenValue(tokenValue);
+    setPayTokenValue(
+      tokenValue !== "" ? (parseFloat(tokenValue) / ratio).toString() : "",
+    );
+  }, []);
+
+  const switchTokens = useCallback(function switchTokens() {
+    temporaryPayTokenValueRef.current = payTokenValue;
+    setPayTokenValue(receiveTokenValue);
+    setReceiveTokenValue(temporaryPayTokenValueRef.current);
+
+    temporaryPayTokenSymbolRef.current = payTokenSymbol;
+    setPayTokenSymbol(receiveTokenSymbol);
+    setReceiveTokenSymbol(temporaryPayTokenSymbolRef.current);
+  }, []);
+
+  return {
+    payTokenSymbol,
+    payTokenValue,
+    receiveTokenSymbol,
+    receiveTokenValue,
+    updatePayTokenValue,
+    updateReceiveTokenValue,
+    switchTokens,
+  };
+}
--- src/app/swap.tsx
+++ src/app/swap.tsx
@@ -8,26 +8,27 @@ import Fieldset from "./ui/fieldset";
 import Form from "./ui/form/form";
 import TokenSwapButton from "./ui/form/token-swap-button";
 import Input from "./ui/input";
+import { useTokenInputs } from "./hooks/token-input";
 
 /**
  * TODO @YohanTz - Use big numbers for calculations
  */
 export default function Swap() {
   const ethBtcRatio = 0.055;
-  // TODO: Type this state stronger than string
-  const [payTokenSymbol, setPayTokenSymbol] = useState("ETH");
-  const [payTokenValue, setPayTokenValue] = useState("");
-
-  // TODO: Type this state stronger than string
-  const [receiveTokenSymbol, setReceiveTokenSymbol] = useState("BTC");
-  const [receiveTokenValue, setReceiveTokenValue] = useState("");
-
-  const temporaryPayTokenValueRef = useRef("");
-  const temporaryPayTokenSymbolRef = useRef("");
 
   const { address } = useAccount();
 
   const { data: balance } = useBalance({ address });
+
+  const {
+    payTokenSymbol,
+    payTokenValue,
+    receiveTokenSymbol,
+    receiveTokenValue,
+    switchTokens,
+    updatePayTokenValue,
+    updateReceiveTokenValue,
+  } = useTokenInputs({ ratio: ethBtcRatio });
 
   const noEnteredAmount =
     payTokenValue.length === 0 && receiveTokenValue.length === 0;
@@ -35,34 +36,6 @@ export default function Swap() {
   const insufficientBalance = balance
     ? parseFloat(balance.formatted) < parseFloat(payTokenValue)
     : true;
-
-  function updatePayTokenValue(tokenValue: string) {
-    setPayTokenValue(tokenValue);
-    setReceiveTokenValue(
-      tokenValue !== ""
-        ? (parseFloat(tokenValue) * ethBtcRatio).toString()
-        : "",
-    );
-  }
-
-  function updateReceiveTokenValue(tokenValue: string) {
-    setReceiveTokenValue(tokenValue);
-    setPayTokenValue(
-      tokenValue !== ""
-        ? (parseFloat(tokenValue) / ethBtcRatio).toString()
-        : "",
-    );
-  }
-
-  function switchTokens() {
-    temporaryPayTokenValueRef.current = payTokenValue;
-    setPayTokenValue(receiveTokenValue);
-    setReceiveTokenValue(temporaryPayTokenValueRef.current);
-
-    temporaryPayTokenSymbolRef.current = payTokenSymbol;
-    setPayTokenSymbol(receiveTokenSymbol);
-    setReceiveTokenSymbol(temporaryPayTokenSymbolRef.current);
-  }
 
   return (
     <Form>
