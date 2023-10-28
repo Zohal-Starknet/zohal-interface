f72f0690882a91eb65a34827671938f5a0d21d58
YohanTz <yohantancrez@gmail.com>
2023-10-28 21:58:38 +0200

    added twitter url

--- src/app/trade/_components/footer.tsx
+++ src/app/trade/_components/footer.tsx
@@ -1,12 +1,15 @@
 import { TwitterIcon } from "@zohal/app/_ui/icons";
 import FooterBlockNumber from "./footer-block-number";
+import { TWITTER_URL } from "@zohal/app/_helpers/socials";
 
 export default function Footer() {
   return (
     <footer className="flex h-[3.25rem] items-center justify-between border-t border-[#2A2E37] px-4">
       <FooterBlockNumber />
       <div className="flex items-center gap-2">
-        <TwitterIcon label="twitter" />
+        <a href={TWITTER_URL} target="_blank">
+          <TwitterIcon label="twitter" />
+        </a>
       </div>
     </footer>
   );
