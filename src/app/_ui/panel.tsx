aa4cd0e7800e6a3bc95678d704458969f90b9d1c
Yohan Tancrez <yohantancrez@gmail.com>
2023-11-02 15:15:18 +0100

    removed unrelevant comment

--- src/app/_ui/panel.tsx
+++ src/app/_ui/panel.tsx
@@ -5,7 +5,6 @@ type PanelProps = {
   className?: string;
 };
 
-// TODO - Use clsx package to resolve className merge issues
 // TODO @YohanTz: take tabsItems as a prop
 export default function Panel(props: PropsWithChildren<PanelProps>) {
   const { children, className } = props;
