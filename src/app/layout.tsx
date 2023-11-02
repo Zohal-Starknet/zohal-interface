02f19cf7f7e341ab8eb2eedce1f607c8bd7fdb58
Yohan Tancrez <yohantancrez@gmail.com>
2023-11-02 16:47:37 +0100

    moved body styles to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html className={robotoMono.className} lang="en">
-      <body>
+      <body className="overflow-hidden">
         <div className="flex h-full flex-col">
           <Providers>
             <Header />
