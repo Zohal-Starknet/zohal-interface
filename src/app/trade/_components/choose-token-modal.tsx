eff96d0f2eeca2e09f6c91473ad200cc01f20a8b
YohanTz <yohantancrez@gmail.com>
2023-11-11 16:30:15 +0300

    improved a bit the design of the modal

---
+++ src/app/trade/_components/choose-token-modal.tsx
@@ -0,0 +1,86 @@
+import { TOKENS, type TokenSymbol } from "@zohal/app/_helpers/tokens";
+import { Dialog, DialogContent, DialogHeader } from "@zohal/app/_ui/Modal";
+import Divider from "@zohal/app/_ui/divider";
+import Input from "@zohal/app/_ui/input";
+import { useMemo, useState } from "react";
+
+type ChooseTokenModalProps = {
+  onOpenChange: (open: boolean) => void;
+  onTokenSymbolChange: (newTokenSymbol: TokenSymbol) => void;
+  open: boolean;
+  tokenSymbol: TokenSymbol;
+};
+export default function ChooseTokenModal(props: ChooseTokenModalProps) {
+  const { onOpenChange, onTokenSymbolChange, open, tokenSymbol } = props;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredTokens = useMemo(() => {
+    return Object.entries(TOKENS).flatMap(([tokenSymbol, tokenInfo]) => {
+      const lowerCaseQuery = searchQuery.toLowerCase();
+
+      const shouldIncludeToken =
+        tokenSymbol.toLowerCase().includes(lowerCaseQuery) ||
+        tokenInfo.name.toLowerCase().includes(lowerCaseQuery) ||
+        tokenInfo.address === lowerCaseQuery;
+
+      if (shouldIncludeToken) {
+        return {
+          tokenSymbol,
+          ...tokenInfo,
+        };
+      }
+
+      return [];
+    });
+  }, [searchQuery]);
+
+  return (
+    <Dialog onOpenChange={onOpenChange} open={open}>
+      <DialogContent className="h-full max-h-[20rem] px-0 pb-0">
+        <DialogHeader className="mx-6 text-left">Select a token</DialogHeader>
+
+        <Input
+          className="mx-6 rounded-md border border-[#363636] bg-[#1b1d22] px-3 py-2 text-base"
+          onChange={(newQuery) => setSearchQuery(newQuery)}
+          placeholder="Search name or paste address"
+          value={searchQuery}
+        />
+
+        <div className="flex flex-col">
+          <Divider />
+
+          <div className="flex flex-col gap-2">
+            {filteredTokens.length ? (
+              filteredTokens.map((filteredToken) => {
+                return (
+                  <button
+                    className="flex items-center justify-between px-6 py-2 transition-colors hover:bg-[#121317]"
+                    key={tokenSymbol}
+                    onClick={() => onTokenSymbolChange(tokenSymbol)}
+                  >
+                    <div className="flex items-center gap-4">
+                      <img
+                        alt={`${filteredToken.name} icon`}
+                        className="h-9 w-9"
+                        src={filteredToken.icon}
+                      />
+                      <div className="flex flex-col text-left">
+                        <h4>{filteredToken?.name}</h4>
+                        <span className="text-xs text-[#A5A5A7]">
+                          {tokenSymbol}
+                        </span>
+                      </div>
+                    </div>
+                    <span>0</span>
+                  </button>
+                );
+              })
+            ) : (
+              <span className="px-6 py-2">No results found.</span>
+            )}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
