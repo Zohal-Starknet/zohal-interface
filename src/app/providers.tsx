9caa821ed26377c413edaa120f1827ec2a8af2a0
Yohan Tancrez <yohantancrez@gmail.com>
2023-11-17 02:23:25 +0100

    bump starknet-react properly

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,32 @@
 "use client";
 
-import { StarknetConfig } from "@starknet-react/core";
-import { connectors } from "@zohal/app/_helpers/connectors";
+import { goerli } from "@starknet-react/chains";
+import {
+  StarknetConfig,
+  argent,
+  braavos,
+  publicProvider,
+} from "@starknet-react/core";
 import { type PropsWithChildren } from "react";
 
 import { ZohalKitProvider } from "./zohal-modal";
+
+const starknetConfig = {
+  chains: [goerli],
+  connectors: [argent(), braavos()],
+  provider: publicProvider(),
+};
 
 export default function Providers(props: PropsWithChildren) {
   const { children } = props;
 
   return (
-    <StarknetConfig autoConnect connectors={connectors}>
+    <StarknetConfig
+      autoConnect
+      chains={starknetConfig.chains}
+      connectors={starknetConfig.connectors}
+      provider={starknetConfig.provider}
+    >
       <ZohalKitProvider>{children}</ZohalKitProvider>
     </StarknetConfig>
   );
