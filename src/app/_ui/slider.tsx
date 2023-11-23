43f97c514ee8e79baa5a425b19aaad0029cf4b4a
YohanTz <yohantancrez@gmail.com>
2023-11-23 19:59:38 +0100

    continued slider

--- src/app/_ui/slider.tsx
+++ src/app/_ui/slider.tsx
@@ -17,10 +17,10 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-green-500">
-      <SliderPrimitive.Range className="absolute h-full bg-red-500" />
+    <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-[#1b1d22]">
+      <SliderPrimitive.Range className="absolute h-full bg-[#363636]" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="border-primary bg-background ring-offset-background focus-visible:ring-ring block h-5 w-5 rounded-full border-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="ring-offset-background focus-visible:ring-ring block h-6 w-6 rounded-md border-2 border-[#363636] bg-[#0d0e11] transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
