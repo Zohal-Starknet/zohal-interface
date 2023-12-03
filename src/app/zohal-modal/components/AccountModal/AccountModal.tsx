82844d1199994eb0640a96c9cd458c0ea3052c1b
Aria Kassaei <arikassaei@gmail.com>
2023-12-03 19:30:55 +0100

    new version of the modal

--- src/app/zohal-modal/components/AccountModal/AccountModal.tsx
+++ src/app/zohal-modal/components/AccountModal/AccountModal.tsx
@@ -8,17 +8,13 @@ import {
   DialogTitle,
 } from "@zohal/app/_ui/Modal";
 import Button from "@zohal/app/_ui/button";
-import {
-  CopyIcon,
-  DisconnectIcon,
-  ExternalLinkIcon,
-} from "@zohal/app/_ui/icons";
+import { CopyIcon, ExternalLinkIcon } from "@zohal/app/_ui/icons";
 import Link from "next/link";
 
-import { useDisplayBalance } from "../../hooks/useDisplayBalance";
 import { useDisplayName } from "../../hooks/useDisplayName";
 
 type AccountModalProps = {
+  /** ID of the connector */
   connectorId: string | undefined;
   /** Callback function called when the modal is closed */
   onClose: () => void;
@@ -31,7 +27,6 @@ export function AccountModal(props: AccountModalProps) {
   const { disconnect } = useConnectors();
 
   const { fullAddress, starkName, truncatedAddress } = useDisplayName();
-  const displayBalance = useDisplayBalance();
 
   const displayName = starkName ?? truncatedAddress;
 
@@ -41,6 +36,7 @@ export function AccountModal(props: AccountModalProps) {
   }
 
   // TODO @YohanTz: rely only on onDisconnect and onConnect in ModalContext.tsx
+  // TODO : handle last transactions done and list them
 
   return (
     <Dialog
@@ -53,21 +49,54 @@ export function AccountModal(props: AccountModalProps) {
         <DialogHeader className="mb-4">
           <DialogTitle>Wallet</DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col items-center">
-          <div className="mb-2 rounded-lg bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-indigo-500 p-4">
-            <img
-              src={
-                connectorId !== undefined
-                  ? connector_id_to_img[connectorId]
-                  : "/logo.svg"
-              }
-              alt={connectorId}
-              className="h-11 w-11"
-            />
+        <div className="flex flex-col gap-4 rounded-md border border-neutral-800 p-4">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2">
+              <img
+                alt={connectorId}
+                className="h-5 w-5"
+                src={
+                  connectorId !== undefined
+                    ? connector_id_to_img[connectorId]
+                    : "/logo.svg"
+                }
+              />
+              <p className="text-center text-lg">{displayName}</p>
+            </div>
+            <Button
+              className="text-xs"
+              onClick={onDisconnect}
+              size="sm"
+              variant="link"
+            >
+              Disconnect
+            </Button>
           </div>
-          <div className="flex items-center gap-1">
-            <p className="text-center text-lg">{displayName}</p>
-            <Button className="h-8 w-8" size="icon" variant="ghost">
+          <div className="flex items-center gap-2">
+            <Button
+              className="inline-flex w-1/2 gap-1 text-xs transition hover:scale-105"
+              onClick={async () => {
+                if (fullAddress === undefined) {
+                  return;
+                }
+
+                // TODO : Add Toasts for Success and Failure cases
+                await navigator.clipboard.writeText(fullAddress);
+              }}
+              size="lg"
+              variant="secondary"
+            >
+              <span>
+                <CopyIcon className="w-4" label="copy-icon" />
+              </span>
+              <span>Copy address</span>
+            </Button>
+            <Button
+              asChild
+              className="inline-flex w-1/2 text-xs transition hover:scale-105"
+              size="lg"
+              variant="secondary"
+            >
               <Link
                 href={`https://starkscan.co/${
                   fullAddress !== undefined ? `/contract/${fullAddress}` : ""
@@ -75,45 +104,18 @@ export function AccountModal(props: AccountModalProps) {
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <ExternalLinkIcon className="h-5 w-5" label="External" />
+                <span className="flex gap-1">
+                  <ExternalLinkIcon className="h-4 w-4" label="External" />
+                  See on the explorer
+                </span>
               </Link>
             </Button>
           </div>
-          <p className="text-muted-foreground text-center text-sm">
-            {displayBalance}
-          </p>
         </div>
-        <div className="flex items-center gap-2">
-          <Button
-            onClick={async () => {
-              if (fullAddress === undefined) {
-                return;
-              }
-
-              // TODO : Add Toasts for Success and Failure cases
-              await navigator.clipboard.writeText(fullAddress);
-            }}
-            className="inline-flex w-1/2 gap-1 transition hover:scale-105"
-            size="lg"
-            variant="secondary"
-          >
-            <span>
-              <CopyIcon className="w-4" label="copy-icon" />
-            </span>
-            <span>Copy address</span>
-          </Button>
-          <Button
-            className="inline-flex w-1/2 gap-1 transition hover:scale-105"
-            onClick={onDisconnect}
-            size="lg"
-            variant="secondary"
-          >
-            <span>
-              <DisconnectIcon className="w-4" label="disconnect-icon" />
-            </span>
-            <span>Disconnect</span>
-          </Button>
-        </div>
+        <h3 className="mt-4">Your transactions</h3>
+        <p className="text-center text-sm text-[#A5A5A7]">
+          No transaction found.
+        </p>
       </DialogContent>
     </Dialog>
   );
