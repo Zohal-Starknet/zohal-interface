63dd6eef25a37d86e7dbf391db8c6fae6a69f2d4
bacharif <arif.bachir@hotmail.com>
2024-07-25 11:35:38 +0200

    feat(mobile): manage theme toggle

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,35 +1,46 @@
-// TODO : See correct usage of Next Image component, Image optimization is not a free feature we actually need to pay for it on Vercel
+import type { Metadata } from "next";
+import { robotoMono } from "@zohal/app/_helpers/fonts";
+import { Toaster } from "sonner";
+import Header from "./_components/header";
+import "./globals.css";
+import Providers from "./providers";
+import { ThemeProvider } from "./_components/theme-provider";
+import { Toast } from "./_ui/toast";
 
-import Link from "next/link";
+export const metadata: Metadata = {
+  description:
+    "Next-generation Perpetual Exchange on Starknet. Revolutionizing User Experience.",
+  title: "Zohal",
+};
 
-import Divider from "../_ui/divider";
-import { ConnectButton } from "../zohal-modal";
-import Navigation from "./navigation";
-import { ThemeToggle } from "./theme-toggle";
-import {FaucetButton} from "@zohal/app/ui/FaucetButton"
-
-/* eslint-disable @next/next/no-img-element */
-export default function Header() {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
-      <header className="flex flex-initial items-center justify-between p-4">
-        <div className="flex items-center gap-4">
-          <Link className="flex-shrink-0" href="/">
-            <img
-              alt="Zohal"
-              className="w-10 rotate-[-35deg] rounded-lg"
-              src="/logo.png"
-            />
-          </Link>
-          <Navigation />
-        </div>
-        <div className="items-cneter flex gap-2">
-          <FaucetButton/>
-          <ConnectButton />
-          <ThemeToggle/>
-        </div>
-      </header>
-      <Divider />
-    </>
+    <html className={robotoMono.className} lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className="lg:overflow-hidden bg-black text-white">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex h-full flex-col min-h-screen">
+            <Providers>
+              <Header />
+              <div className="flex-auto">
+                {children}
+              </div>
+              <Toaster position="bottom-right" />
+            </Providers>
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
